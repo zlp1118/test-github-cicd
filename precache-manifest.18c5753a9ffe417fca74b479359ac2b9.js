aa9bd149aeea39eb18db4d3c3b73427133cc76ba
zlp1118 <812575343@qq.com>
2020-03-07 08:40:37 +0000

    Deploying to gh-pages from  - e2b315612b4f1de8ac212213715bd4688fbf90fd 🚀

---
+++ precache-manifest.18c5753a9ffe417fca74b479359ac2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a748c95ca156cd93f18af14810fc0a26",
+    "url": "/test-github-cicd/index.html"
+  },
+  {
+    "revision": "b36bdb6f4e7b23452369",
+    "url": "/test-github-cicd/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "d4a85fcc33179424b1c1",
+    "url": "/test-github-cicd/static/js/2.fdcb5427.chunk.js"
+  },
+  {
+    "revision": "b36bdb6f4e7b23452369",
+    "url": "/test-github-cicd/static/js/main.9169a459.chunk.js"
+  },
+  {
+    "revision": "627d5ad501fb9a91fe04",
+    "url": "/test-github-cicd/static/js/runtime~main.2caeb623.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-github-cicd/static/media/logo.5d5d9eef.svg"
+  }
+]);
